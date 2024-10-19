02986768f763be3fa79169b3a0f6eb428897bfa7
SAI KIRAN <saikiran4824@gmail.com>
2024-10-20 00:21:59 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClYPy3nscXi_hcjgcjgfimyC-t3cgCrryPENhIUhA",
-  authDomain: "fir-auth-9c2a8.firebaseapp.com",
-  projectId: "fir-auth-9c2a8",
-  storageBucket: "fir-auth-9c2a8.appspot.com",
-  messagingSenderId: "754200905222",
-  appId: "1:754200905222:web:0c220aec7314a569676587",
-  measurementId: "G-GZKB7QKWBM"
+  apiKey: // put your key
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
